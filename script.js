bf9d274934b27cb92058289e1061d7dc2d716462
Kuba Wysocki <jestemjakubwysocki@gmail.com>
2018-10-25 22:20:40 +0200

    naprawa błędu spowalniającego kod i inne poprawki

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload=()=>{
-    let plus=document.getElementById('addTask');
+    const plus=document.getElementById('addTask');
     plus.addEventListener('click',addTaskUtility);
 
-    let reset=document.getElementById('btnR');
+    const reset=document.getElementById('btnR');
     reset.addEventListener('click',()=>{
         if(document.querySelectorAll('.taskItem').length>0){
             if(confirm('Czy na pewno chcesz usunąć wszystko?')){
@@ -11,46 +11,71 @@ window.onload=()=>{
             }
         }
     });
-    let delDone=document.getElementById('btnG');
+    const delDone=document.getElementById('btnG');
     delDone.addEventListener('click',()=>{
-        let done=document.querySelectorAll('.ticActive');
+        const done=document.querySelectorAll('.ticActive');
         for (i=0;i<done.length;i++){
             done[i].parentNode.parentNode.parentNode.parentNode.removeChild(done[i].parentNode.parentNode.parentNode)
         }
         counter();
         delDone.className='btn btn-secondary';
     });
-    let enterTask=document.getElementById('task');
+    const enterTask=document.getElementById('task');
     enterTask.focus();
     enterTask.addEventListener('keydown',(e)=>{
         if(e.key=="Enter") addTaskUtility();
     });
-    let memo= localStorage.getItem('list');
+    const memo= localStorage.getItem('list');
     document.getElementById('taskList').innerHTML=memo;
     makeActions();
     counter();
+}
+function makeActions(){
+    const trash=document.querySelectorAll('.trash');
+    const allTic=document.querySelectorAll('.tic');
+    const edit=document.querySelectorAll('.edit');
+    const addSubList=document.querySelectorAll('.addSubList');
+    const arrow=document.querySelectorAll('.arr');
+    const subItem=document.querySelectorAll('.addSubItem');
+    const inputSubList=document.querySelectorAll('.inputSubList');
+
+    for (i=0;i<trash.length;i++){
+        if(trash[i])trash[i].addEventListener('click',erase);
+        if(allTic[i])allTic[i].addEventListener('click',ticAction);
+        if(edit[i])edit[i].addEventListener('click',redoMode);
+        if(addSubList[i])addSubList[i].addEventListener('click',subList);
+        if(arrow[i]) arrow[i].addEventListener('click',hideSubList);
+        if(subItem[i]) subItem[i].addEventListener('click',function(){addSubItem(this.previousSibling)});
+        if(inputSubList[i])inputSubList[i].addEventListener('keydown',function(e){
+            if(e.key=="Enter") addSubItem(this);
+        });
+    }
 }
 function addTaskUtility() {
     const trash=new Image();
         trash.src='img/trash.png';
         trash.className='trash';
+        trash.addEventListener('click',erase);
 
     let actualTask=document.getElementById('task');
-    let taskList=document.getElementById('taskList');
+    const taskList=document.getElementById('taskList');
     
-    let newTask= document.createElement('li');
-    let taskItem=document.createElement('div');
+    const newTask= document.createElement('li');
+    const taskItem=document.createElement('div');
         taskItem.className='taskItem';
-    let inputArea=document.createElement('div');
+    const inputArea=document.createElement('div');
         inputArea.className='inputArea';
-    let actionArea=document.createElement('div');
+    const actionArea=document.createElement('div');
         actionArea.className='actionArea';
-    let tic=document.createElement('div');
+    const tic=document.createElement('div');
         tic.className='tic';
-    let pen=document.createElement('div');
+        tic.addEventListener('click',ticAction);
+    const pen=document.createElement('div');
         pen.classList.add('edit','pen');
-    let plus=document.createElement('div');
+        pen.addEventListener('click',redoMode);
+    const plus=document.createElement('div');
         plus.classList.add('plus','addSubList');
+        plus.addEventListener('click',subList);
 
     if (actualTask.value){
         inputArea.appendChild(document.createTextNode(actualTask.value));
@@ -66,33 +91,11 @@ function addTaskUtility() {
         
         taskList.insertBefore(newTask, taskList.childNodes[0]);
         counter();
-        makeActions();
         actualTask.value='';
     }
 }
-function makeActions(){
-    let minus=document.querySelectorAll('.trash');
-    let allTic=document.querySelectorAll('.tic');
-    let edit=document.querySelectorAll('.edit');
-    let addSubList=document.querySelectorAll('.addSubList');
-    let arrow=document.querySelectorAll('.arr');
-    let subItem=document.querySelectorAll('.addSubItem');
-    let inputSubList=document.querySelectorAll('.inputSubList');
-
-    for (i=0;i<minus.length;i++){
-        if(minus[i])minus[i].addEventListener('click',erase);
-        if(allTic[i])allTic[i].addEventListener('click',ticAction);
-        if(edit[i])edit[i].addEventListener('click',redoMode);
-        if(addSubList[i])addSubList[i].addEventListener('click',subList);
-        if(arrow[i]) arrow[i].addEventListener('click',hideSubList);
-        if(subItem[i]) subItem[i].addEventListener('click',function(){addSubItem(this.previousSibling)});
-        if(inputSubList[i])inputSubList[i].addEventListener('keydown',function(e){
-            if(e.key=="Enter") addSubItem(this);
-        });
-    }
-}
 function counter(){
-    let taskCo=document.getElementById('counter');
+    const taskCo=document.getElementById('counter');
     taskCo.innerHTML=document.querySelectorAll('.taskItem').length;
 }
 function erase(){
@@ -101,14 +104,14 @@ function erase(){
 }
 function ticAction(){
     this.classList.toggle('ticActive');
-    let btn=document.getElementById('btnG');
-    let ticks=document.querySelectorAll('.ticActive');
+    const btn=document.getElementById('btnG');
+    const ticks=document.querySelectorAll('.ticActive');
     if (ticks.length>0) btn.className='btn btn-success';
     else btn.className='btn btn-secondary';
 }
 function redoMode(){
     let oldTask=this.parentNode.previousSibling.firstChild.nodeValue;
-    let editInput=document.createElement('input');
+    const editInput=document.createElement('input');
         editInput.placeholder='Aktualizuj zadanie';
         editInput.value=oldTask;
     editInput.addEventListener('keydown',(e)=>{
@@ -135,18 +138,22 @@ function redoMode(){
     counter();
 }
 function subList(){
-    let subBoard=document.createElement('div');
+    const subBoard=document.createElement('div');
         subBoard.classList.add('subBoard');
-    let orderLi=document.createElement('ol');
+    const orderLi=document.createElement('ol');
         subBoard.appendChild(orderLi);
-    let box=document.createElement('div');
+    const box=document.createElement('div');
         box.className='box';
-    let inputSubList=document.createElement('input');
+    const inputSubList=document.createElement('input');
         inputSubList.className='inputSubList';
         inputSubList.placeholder='Dodaj podpunkt do zadania';
+        inputSubList.addEventListener('keydown',function(e){
+            if(e.key=="Enter") addSubItem(this);
+        });
         box.appendChild(inputSubList);
-    let plus=document.createElement('div');
+    const plus=document.createElement('div');
         plus.classList.add('plus','addSubItem');
+        plus.addEventListener('click',function(){addSubItem(this.previousSibling)});
         box.appendChild(plus);
 
         subBoard.appendChild(box);
@@ -154,11 +161,11 @@ function subList(){
     this.parentNode.parentNode.parentNode.appendChild(subBoard);
     this.removeEventListener('click',subList);
     this.className='arr';
+    this.addEventListener('click',hideSubList);
     inputSubList.focus();
-    makeActions();
 }
 function hideSubList(){
-    let subBoard=this.parentNode.parentNode.nextSibling;
+    const subBoard=this.parentNode.parentNode.nextSibling;
     subBoard.classList.toggle('hide');
     this.classList.toggle('rotor');
 }
@@ -166,15 +173,17 @@ function addSubItem(that){
     const trash=new Image();
         trash.src='img/trash.png';
         trash.className='trash';
-    let tic=document.createElement('div');
+        trash.addEventListener('click',erase);
+    const tic=document.createElement('div');
         tic.className='tic';
+        tic.addEventListener('click',ticAction);
 
-    let subItem=document.createElement('li');
-    let subInputArea=document.createElement('div');
+    const subItem=document.createElement('li');
+    const subInputArea=document.createElement('div');
         subInputArea.className='subInputArea';
-    let subActionArea=document.createElement('div');
+    const subActionArea=document.createElement('div');
         subActionArea.className='subActionArea';
-    let subTaskItem=document.createElement('div');
+    const subTaskItem=document.createElement('div');
         subTaskItem.className='subTaskItem';
 
     if(that.value){
@@ -187,10 +196,9 @@ function addSubItem(that){
         that.parentNode.previousSibling.appendChild(subItem);
     }
     that.value='';
-    makeActions();
 }
 window.onunload=()=>{
-    let unsavedTasks= document.querySelectorAll('.save');
+    const unsavedTasks= document.querySelectorAll('.save');
     for(i=0;i<unsavedTasks.length;i++) {unsavedTasks.parentNode.parentNode.parentNode.removeChild(unsavedTasks[i])}
     localStorage.setItem('list',document.getElementById('taskList').innerHTML)
 }
